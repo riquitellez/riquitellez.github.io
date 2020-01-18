1fd54db28ec0906d66c57004a4b177ddab28872a
riquitellez <riquitellez@gmail.com>
2020-01-18 20:33:06 +0100

    week 2 assignment

---
+++ js/designprinciple.js
@@ -0,0 +1,7 @@
+// JS to get current date
+
+var today = new Date();
+var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+ today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+document.getElementById("currentDate").innerHTML = "Last update: " + dateTime;
